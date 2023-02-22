541fd41e76d13b55ce7c4af6e13b38772ffe8d26
Костин Иван <i.kostin@cognitivepilot.com>
2023-02-22 20:14:05 +0300

    second sample + new gitignore

---
+++ lecture0/sample1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("enter your name:");
+string username = Console.ReadLine();
+Console.WriteLine(String.Format("Hello, {0}!", username));
