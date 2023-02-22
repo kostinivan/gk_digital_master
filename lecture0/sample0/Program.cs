c62f719d8ec03bed29f650736bef5965f412e17b
Костин Иван <i.kostin@cognitivepilot.com>
2023-02-22 20:05:38 +0300

    elemntary sample c#

---
+++ lecture0/sample0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sample0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 Hello World!");
+        }
+    }
+}
