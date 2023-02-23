afa0ce2da72cf3b840a9a9b7c477139f0f1849e8
Костин Иван <i.kostin@cognitivepilot.com>
2023-02-23 15:36:14 +0300

    lecture 0 done

---
+++ meet_csharp/lecture0/sampl4_random/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+double lhs = new Random().Next(0,10);
+double rhs = new Random().Next(0,10);
+Console.WriteLine(String.Format("the {0}", lhs / rhs));
