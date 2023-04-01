183bb2108490ff11fecc45fe8892527905e51a21
Костин Иван <i.kostin@cognitivepilot.com>
2023-04-01 17:05:40 +0300

    gitignore apdate

--- inter_sert_1/vecstr/src/vecstr.cpp
+++ inter_sert_1/vecstr/src/vecstr.cpp
@@ -1,9 +1,19 @@
 #include "../vecstr.hpp"
-#include <iostream>
 
 using namespace std;
 
+vector<string> VectorGroomer::get_result(){
+    perform_groom();
+    return to_groom_;
+}
 
-int main(){
-    cout << "hello" << endl;
+void VectorGroomer::perform_groom(){
+    vector<string> result;
+    result.reserve(to_groom_.size());
+    for (int i = 0; i < to_groom_.size(); i++){
+        if (to_groom_.at(i).size() <= max_length_){
+            result.push_back(to_groom_.at(i));
+        }
+    }
+    to_groom_ = result;
 }
--- inter_sert_1/vecstr/vecstr.hpp
+++ inter_sert_1/vecstr/vecstr.hpp
@@ -1,3 +1,20 @@
 #pragma once
 #include <string>
+#include <vector>
 
+class VectorGroomer{
+    public:
+        VectorGroomer() = delete;
+        VectorGroomer(std::vector<std::string> input, uint64_t input_max_length) : 
+            to_groom_(input),
+            max_length_(input_max_length)
+            {};
+        ~VectorGroomer() = default;
+
+        std::vector<std::string> get_result();
+
+    private:
+        void perform_groom();
+        std::vector<std::string> to_groom_;
+        uint64_t max_length_ = 3;
+};
